d29a855ec48151258b18b454d94ffc3abcbc3919
Rolando <rolando.code@gmail.com>
2022-12-31 18:58:58 -0600

    number of tries

--- python_curso/Conditionals, bucles, functions/loops_exercise2.py
+++ python_curso/Conditionals, bucles, functions/loops_exercise2.py
@@ -5,11 +5,13 @@ randomColorIndex =  random.randint(0, len(colors)-1)
 randomColor = colors[randomColorIndex]
 
 userColor = input("Enter a color to guess: ")
+tries = 1
 
 while True:
     if (userColor == randomColor):
-        print("You got it!")
+        print("You got it! in ", tries, " tries")
         break
     else:
         userColor = input("Try again: ")
+        tries = tries + 1
     
