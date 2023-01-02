05970113c94118468c0dc2e165d5f0965468c2bd
rolandocode <rolando.code@gmail.com>
2023-01-02 13:39:17 -0600

    remove comments for refactor quiz

--- python_curso/Modules/quiz_game.py
+++ python_curso/Modules/quiz_game.py
@@ -17,19 +17,6 @@ def playGameQuestion(question, i, winPoints) :
         print("Bad andswer :C")
         return 0
 
-    # for j in range(len(options_answers)):
-    #     print (j+1, ") ", options_answers[j])
-    
-    # user_answer = int (input ("Choice the number for the answer: "))
-    # correct_asnwer = question["results"][i]["correct_answer"]
-    # given_answer = options_answers[user_answer-1]
-    # if (given_answer == correct_asnwer):
-    #     print("Correct! + ", winPoints, "points")
-    #     return winPoints
-    # else:
-    #     print("Bad andswer :C")
-    #     return 0
-
 def printOptions (options_answers):
     for j in range(len(options_answers)):
         print (j+1, ") ", options_answers[j])
@@ -39,7 +26,6 @@ def isCorrectAndswer(question, options_answers):
     correct_asnwer = question["results"][i]["correct_answer"]
     given_answer = options_answers[user_answer-1]
     if (given_answer == correct_asnwer):
-        # print("Correct! + ", winPoints, "points")
         return True
     else:
         return False
@@ -57,22 +43,6 @@ while continue_play_game == True:
     for i in range(0,len(question["results"])):
         points_earned_this_thime = playGameQuestion(question, i, points_to_earn)
         player_points = player_points + points_earned_this_thime
-        # print("Q# ", i+1, ": ", question["results"][i]["question"]) 
-        # options_answers = question["results"][i]["incorrect_answers"]
-        # options_answers.append(question["results"][i]["correct_answer"])
-        # random.shuffle(options_answers)
-
-        # for j in range(len(options_answers)):
-        #     print (j+1, ") ", options_answers[j])
-        
-        # user_answer = int (input ("Choice the number for the answer: "))
-        # correct_asnwer = question["results"][i]["correct_answer"]
-        # given_answer = options_answers[user_answer-1]
-        # if (given_answer == correct_asnwer):
-        #     print("Correct! + ", points_to_earn, "points")
-        #     player_points = player_points + points_to_earn
-        # else:
-        #     print("Bad andswer :C")
     
     print ("Points earned at now: ", player_points)
     continue_aswer = input("Do you want to continue playing? (Y/N): ")
